bde0d14d532b23d9741909baf1a3ed0db5e70652
John Alexander Torres <jalexandert@gmail.com>
2013-05-15 12:12:46 -0500

    test image boostrap

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 angular.module('stupscataloge', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/stups', {templateUrl: 'partials/stups-list.html',   controller: StubsListCtrl}).
-      when('/stups/:phoneId', {templateUrl: 'partials/stup-detail.html', controller: StubsDetCtrl}).
+      when('/stups',	{templateUrl: 'partials/stups-list.html',	
+      					controller: StubsListCtrl}).
+
+      when('/stups/:phoneId',	{templateUrl: 'partials/stup-detail.html', 
+      							controller: StubsDetCtrl}).
+      
       when('/404', {templateUrl: 'partials/error.html', controller: ErrorCtrl}).
       otherwise({redirectTo: '/stups'});
 }]);
