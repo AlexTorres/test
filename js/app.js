61e9c793cdc7cfa275d2c9d945b53c0fc576eaab
John Alexander Torres <Jalexandert@gmail.com>
2013-05-14 23:27:43 -0500

    ahora si

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ angular.module('stupscataloge', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/stups', {templateUrl: 'partials/stups-list.html',   controller: StubsListCtrl}).
-      when('/stups/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: StubsDetCtrl}).
+      when('/stups/:phoneId', {templateUrl: 'partials/stup-detail.html', controller: StubsDetCtrl}).
       otherwise({redirectTo: '/stups'});
 }]);
