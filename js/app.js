d53e26951b726fcad26cf0af5013faedc8cee4fa
John Alexander Torres <Jalexandert@gmail.com>
2013-05-14 23:23:04 -0500

    sera

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ angular.module('stupscataloge', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/stups', {templateUrl: 'partials/stups-list.html',   controller: StubsListCtrl}).
-      when('/stup/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: StubsDetCtrl}).
+      when('/stups/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: StubsDetCtrl}).
       otherwise({redirectTo: '/stups'});
 }]);
